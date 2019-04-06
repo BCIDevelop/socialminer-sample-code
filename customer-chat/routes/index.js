453c1a202d43ecd565309842bd4b25cfe36bab45
luis lopez <luis.lopez@utec.edu.pe>
2019-04-06 15:12:14 -0500

    Only Facebook App

--- customer-chat/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'SocialMiner Chat Sample' });
-});
-
-module.exports = router;
